4efee883d2ccf32fc59c2e1e94e43367e1774db3
John Aguirre <john.p.aguirre@norcrossvoip.com>
2015-08-08 23:17:18 -0400

    Added angularApp file and the first controller.  Added fields and modified the page to add Lodging dates.

---
+++ 324THLHI/Scripts/AngularApp.js
@@ -0,0 +1,27 @@
+﻿angular.module('LIKApp', [])
+  .controller('GenerateLIKPDF', ['$scope', '$http' ,GenerateLIKPDF]);
+
+function GenerateLIKPDF($scope, $http) {
+    $scope.LodgingDate = [];
+    $scope.LodgingDate[0] = ""; 
+    $scope.Unit = "";
+    $scope.Name = "";
+    $scope.Rank = "";
+    $scope.Address = "";
+    $scope.CSZ = "";
+    $scope.PhoneAreaCode = "";
+    $scope.Phone = "";
+    $scope.WorkPhoneAreaCode = "";
+    $scope.WorkPhone = "";
+    $scope.Initials = "";
+    $scope.Mileage = "";
+    $scope.Email = "";
+
+    $scope.AddLodgingDate = function () {
+        $scope.LodgingDate.push("");
+    }
+
+    $scope.GeneratePDF = function () {
+        $scope.LodgingDate.push("");
+    }
+}
